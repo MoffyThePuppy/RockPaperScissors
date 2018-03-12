b2addac57b3c6ea2ca4839a68a9e38ed2c93448f
Flores <0084216@students.dadeschools.net>
2018-03-12 12:02:05 -0400

    Starting Project

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 0084216
+ * Date: 3/12/2018
+ * Time: 11:54 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RockPaperScissors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
